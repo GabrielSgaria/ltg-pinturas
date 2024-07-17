60964b9c4fbf0ebfb6500e0f440611b3fb2a9501
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-07-17 14:28:50 -0300

    feat fotos

--- src/components/carrousel-portfolio.tsx
+++ src/components/carrousel-portfolio.tsx
@@ -34,18 +34,21 @@ export function ClientsCarrousel() {
           { src: "/image/jobs/6.jpeg"},
           { src: "/image/jobs/7.jpeg"},
           { src: "/image/jobs/8.jpeg"},
-  
+          { src: "/image/jobs/9.jpeg"},
           { src: "/image/jobs/10.jpeg"},
           { src: "/image/jobs/11.jpeg"},
           { src: "/image/jobs/12.jpeg"},
           { src: "/image/jobs/13.jpeg"},
-      
+          { src: "/image/jobs/14.jpeg"},
+          { src: "/image/jobs/15.jpeg"},
+          { src: "/image/jobs/16.jpeg"},
+     
 
         ].map((project, index) => (
           <SplideSlide key={index} className="flex justify-center items-center py-5 md:w-[650px] md:h-[450px] max-h-[650px]">
 
-            <div className="w-full h-[100%] rounded-xl overflow-hidden">
-              <Image src={project.src} width={700} height={700} quality={100} priority={true} alt={`Project ${index + 1}`} className="w-full h-full object-fill" />
+            <div className="w-full max-w-[544px] h-[100%] rounded-xl overflow-hidden">
+              <Image src={project.src} width={900} height={900} quality={100} priority={true} alt={`Project ${index + 1}`} className="w-full  h-full object-fill" />
             </div>
 
           </SplideSlide>
